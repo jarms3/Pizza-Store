427d4b15eef83d9b81721ee2ab91b0f49c39933c
jarms3 <35812722+jarms3@users.noreply.github.com>
2018-11-26 12:40:30 -0500

    Initial commit

---
+++ PizzaStore/storescript.js
@@ -0,0 +1,155 @@
+window.onload = function(){
+    var total = document.getElementById('total');
+    var totBTN = document.createElement("BUTTON");
+    var upBTN = document.createElement("BUTTON");
+    var getCust = document.createElement("BUTTON");
+    var totTXT = document.createTextNode("Get Price");
+    var update = document.createTextNode("Update");
+    var cust = document.createTextNode("Get Customers");
+    var inputOne = document.createElement("input");
+    var plist = document.getElementById("price");
+    var orders = document.getElementById("orders");
+    var status = document.getElementById("status");
+    var customers = document.getElementById("customers");
+    var getem = document.getElementById("getCust");
+    var inputTwo = document.createElement("input");
+    var inputThree = document.createElement("input");
+    inputOne.type = "text";
+    inputTwo.type = "text";
+    inputThree.type = "text";
+    status.appendChild(inputTwo);
+    status.appendChild(inputThree);
+    status.appendChild(upBTN);
+    totBTN.appendChild(totTXT);
+    upBTN.appendChild(update);
+    getCust.appendChild(cust);
+    totBTN.addEventListener("click", getTotal);
+    upBTN.addEventListener("click", updateStatus);
+    getCust.addEventListener("click", getCustomers);
+    total.appendChild(inputOne);
+    total.appendChild(totBTN);
+    getem.appendChild(getCust);
+
+    setInterval(function get(){
+        var http = new XMLHttpRequest(); //set up the request
+        url = 'http://localhost:8080/api';
+
+        http.open('GET', url, true);
+
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4 && http.status == 200) {
+                if(orders.childNodes[0] != null){
+                    while (orders.firstChild) {
+                        orders.removeChild(orders.firstChild);
+                    }
+                }
+                var res = JSON.parse(http.response);
+
+                for(var j = 0; j < res.length; j++){
+                    var li = document.createElement("li");
+                    var oTxt = document.createTextNode("Order Id : " + res[j].orderID + " ");
+                    var amountTxt = document.createTextNode("Amount Total : $" + res[j].amountTotal + " ");
+                    var status = document.createTextNode("Status : " + res[j].status + " ");
+                    li.appendChild(oTxt);
+                    li.appendChild(amountTxt);
+                    li.appendChild(status);
+                    orders.appendChild(li);
+                }
+            }
+        }
+        http.send(null);
+    }, 2000);
+
+    function getTotal(){
+        var http = new XMLHttpRequest(); //set up the request
+        url = 'http://localhost:8080/api/' + inputOne.value;
+  
+        http.open('GET', url, true);
+
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4 && http.status == 200) {
+                if(plist.childNodes[0] != null){
+                    while (plist.firstChild) {
+                        plist.removeChild(plist.firstChild);
+                    }
+                }
+                var response = JSON.parse(http.response);
+
+                var li = document.createElement("li");
+                li.appendChild(document.createTextNode("Total : $ " + response[0].amountTotal));
+                plist.appendChild(li);
+            }
+        }
+
+        http.send(null);
+    }
+
+    function updateStatus(){
+        var http = new XMLHttpRequest();
+        url = 'http://localhost:8080/api/' + inputTwo.value;   
+        var params = 'status=' + "'" + inputThree.value + "'";
+
+        http.open('POST', url, true);
+
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4 && http.status == 200) {
+                console.log(http.responseText);
+            }
+        }
+        http.send(params);
+    }
+
+    function getCustomers(){
+        var http = new XMLHttpRequest(); //set up the request
+        url = 'http://localhost:8080/api/customers';
+  
+        http.open('GET', url, true);
+
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4 && http.status == 200) {
+                if(customers.childNodes[0] != null){
+                    while (customers.firstChild) {
+                        customers.removeChild(customers.firstChild);
+                    }
+                }
+
+                var table = document.createElement("TABLE");
+                var attributes = document.createElement("TR");
+                var name = document.createElement("TD");
+                var n = document.createTextNode("Name");
+                var streetNum = document.createElement("TD");
+                var sn = document.createTextNode("Street Number");
+                var streetName = document.createElement("TD");
+                var sna = document.createTextNode("Street Name");
+                streetName.appendChild(sna);
+                streetNum.appendChild(sn);
+                name.appendChild(n);
+                attributes.appendChild(name);
+                attributes.appendChild(streetNum);
+                attributes.appendChild(streetName);
+                table.appendChild(attributes);
+                customers.appendChild(table);
+                var res = JSON.parse(http.response);
+                for(var j = 0; j < res.length; j++){
+                    var data = document.createElement("TR");
+                    var value = document.createElement("TD");
+                    value.appendChild(document.createTextNode(res[j].name));
+                    console.log(res[j].name);
+                    var snum = document.createElement("TD");
+                    snum.appendChild(document.createTextNode(res[j].streetNum));
+                    var sname = document.createElement("TD");
+                    sname.appendChild(document.createTextNode(res[j].streetName));
+                    data.appendChild(value);
+                    data.appendChild(snum);
+                    data.appendChild(sname);
+                    table.appendChild(data);
+                }
+            }
+            
+        }
+
+        http.send(null);
+    }
+}
---
+++ Server/server.js
@@ -0,0 +1,89 @@
+var express    = require('express');        // call express
+var app        = express();// define our app using express
+var bodyParser = require('body-parser');
+
+var mysql = require('mysql');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "pizza"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  con.query("SET FOREIGN_KEY_CHECKS=0");
+
+  app.use('/api', router);
+
+  router.use(function(req, res, next) {
+    // do logging
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.header("Access-Control-Allow-Headers", "Origin, XRequested-With,Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, PATCH, GET, PUT, DELETE, OPTIONS');
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+  });
+
+  router.get('/', function(req, res) {
+    con.query("SELECT * FROM pizza.order", function(err, result){
+        if (err) throw err;
+        res.json(result);
+    });   
+  });
+
+  router.route('/customers')
+
+  .get(function(req, res){
+      con.query("SELECT customer.name, address.streetNum, address.streetName FROM pizza.customer inner join pizza.address on address.addressID = customer.addressID inner join pizza.order on order.customerID = customer.customerID WHERE order.status = 'PREP' OR order.status = 'COOK'", function(err, result){
+          res.json(result);
+      })
+  })
+
+  router.route('/:order_id')
+
+  .get(function(req, res) {
+    con.query("UPDATE pizza.order SET amountTotal = (SELECT SUM(price*quantity) FROM pizza.pizzas inner join pizza.pizzasinorder on pizzasinorder.pizzaName = pizzas.pizzaName WHERE orderID = " + req.params.order_id + ") WHERE orderID = " + req.params.order_id, function(err, result){
+        if(err) throw err;
+    });
+
+    con.query("SELECT amountTotal FROM pizza.order WHERE orderID = " + req.params.order_id, function(err, result){
+        if(err) throw err;
+
+        res.json(result);
+    });
+  })
+
+  .post(function(req, res){
+      con.query("UPDATE pizza.pizzasinorder SET status = " + req.body.status + " WHERE orderID = " + req.params.order_id, function(err, result){
+          if(err) throw err;
+      });
+
+      con.query("UPDATE pizza.order SET status = " + req.body.status + " WHERE orderID = " + req.params.order_id, function(err, result){
+        if(err) throw err;
+        console.log(req.body.status);
+        if(req.body.status == "'DONE'"){
+            con.query("DELETE pizza.order, pizza.pizzasinorder FROM pizza.order inner join pizza.pizzasinorder on pizzasinorder.orderID = order.orderID WHERE order.status = 'DONE' AND pizzasinorder.status = 'DONE'", function(error, resp){
+                if(error) throw error;
+                res.json(resp);
+            });
+        }
+      });
+
+  })
+
+  
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
